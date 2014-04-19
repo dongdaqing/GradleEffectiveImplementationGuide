2c67dc5e4c245ebf4614a1bf5349f6cb6bbaef2e
kazurayam <kazuaki.urayama@gmail.com>
2014-04-19 09:15:48 +0900

    add CreatingANewSourceSet.gradle file

--- chap04/src/main/java/gradle/sample/Sample.java
+++ chap04/src/main/java/gradle/sample/Sample.java
@@ -2,11 +2,11 @@ package gradle.sample;
 
 import java.util.ResourceBundle;
 
-public class Sample {
+public class Sample implements ReadWelcomeMessage {
     public Sample() {}
     public String getWelcomeMessage() {
-	final ResourceBundle resourceBundle = ResourceBundle.getBundle("messages");
-	final String message = resourceBundle.getString("welcome");
-	return message;
+        final ResourceBundle resourceBundle = ResourceBundle.getBundle("messages");
+        final String message = resourceBundle.getString("welcome");
+        return message;
     }
 }

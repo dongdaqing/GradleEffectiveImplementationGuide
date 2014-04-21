2db665d0b46cb4defdb3bc9ed783e056d0c28d6e
kazurayam <kazuaki.urayama@gmail.com>
2014-04-21 23:17:49 +0900

    add chap10

---
+++ chap06/src/main/java/gradle/sample/Sample.java
@@ -0,0 +1,13 @@
+package gradle.sample;
+
+import java.util.ResourceBundle;
+
+public class Sample {
+    public Sample() {
+    }
+    public String getWelcomeMessage() {
+        final ResourceBundle resourceBundle = ResourceBundle.getBundle("gradle.sample.messages");
+        final String message = resourceBundle.getString("welcome");
+        return message;
+    }
+}
---
+++ chap10/CreatingATaskInThePrjojectSourceDirectory/buildSrc/src/main/groovy/sample/InfoTask.groovy
@@ -0,0 +1,13 @@
+package sample
+
+import org.gradle.api.*
+import org.gradle.api.tasks.*
+
+class InfoTask extends DefaultTask {
+  String prefix = 'Current Gradle version'
+
+  @TaskAction
+  def info() {
+    println "${prefix}: ${project.gradle.gradleVersion}"
+  }
+}
